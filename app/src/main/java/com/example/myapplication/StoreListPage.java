8de18914dd2dc40445640d57309944eac24a73e0
이진 <ctce7226@gmail.com>
2023-12-09 03:46:02 +0900

    복덩이

---
+++ app/src/main/java/com/example/myapplication/StoreListPage.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StoreListPage extends AppCompatActivity {
+    Button beforeButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_list_page);
+    }
+
+    public void CategoryButtonClicked(View v) {
+        if(beforeButton != null){
+            beforeButton.setBackgroundResource(R.drawable.button_design);
+            beforeButton.setTextColor(Color.parseColor("#000000"));
+        }
+        Button button = (Button) findViewById(v.getId());
+        button.setBackgroundResource(R.drawable.clicked_button_design);
+        button.setTextColor(Color.parseColor("#FFFFFF"));
+        beforeButton = button;
+    }
+}
