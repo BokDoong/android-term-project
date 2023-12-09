e8c2e9cece7a221b6c68dae95af758b3080fa3ab
b0310y <nancy30@naver.com>
2023-12-09 22:40:10 +0900

    추가dfdf

---
+++ app/src/main/java/com/example/myapplication/StoreDetailPage.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreDetailPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_detail_page);
+    }
+}
