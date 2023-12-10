2ff192089acf7af80e15fefef3559cf4366e8260
jinuk <cimaron713@gmail.com>
2023-12-11 00:13:56 +0900

    굿

--- app/src/main/java/com/example/myapplication/RealMainPage.java
+++ app/src/main/java/com/example/myapplication/RealMainPage.java
@@ -1,15 +1,19 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.GridView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RealMainPage extends AppCompatActivity {
-
+    DBHelper helper;
+    SQLiteDatabase db;
     // 10개의 이미지 및 텍스트를 어댑터에 추가
     int[] imageIds = {R.drawable.korean, R.drawable.chinese, R.drawable.western,
             R.drawable.japanese, R.drawable.cafe, R.drawable.bar,
@@ -29,6 +33,14 @@ public class RealMainPage extends AppCompatActivity {
         gridView = (GridView) findViewById(R.id.grid_view);
         imageTextAdapter = new ImageTextAdapter(this, imageIds, textArray);
 
+        // DB Helper
+        helper = new DBHelper(this);
+        try {
+            db = helper.getWritableDatabase();
+        } catch (SQLiteException ex) {
+            db = helper.getReadableDatabase();
+        }
+
         gridView.setAdapter(imageTextAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -41,6 +53,12 @@ public class RealMainPage extends AppCompatActivity {
     }
     public void onClickMap(View v){
         Intent myIntent = new Intent(RealMainPage.this, MapActivity.class);
+        startActivity(myIntent);
+    }
+    public void onClickSearch(View v){
+        EditText search = (EditText)findViewById(R.id.serchbar);
+        Intent myIntent = new Intent(RealMainPage.this, StoreDetailPage.class);
+        myIntent.putExtra("가게", search.getText().toString());
         startActivity(myIntent);
     }
 }
